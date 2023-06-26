61152b38cb05d24ce3ef49921a2fd1c469e8583f
Taavetti Yilnan <dyildum@gmail.com>
2023-06-26 15:11:12 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,17 +7,17 @@
  */
 void puts2(char *str)
 {
-    int i;
-    int j = 0;
+	int i;
+	int j = 0;
 
-    while (str[j] != '\0')
-    {
-        j++;
-    }
+	while (str[j] != '\0')
+	{
+		j++;
+	}
 
-    for (i = 0; i < j; i += 2)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+		_putchar('\n');
 }
