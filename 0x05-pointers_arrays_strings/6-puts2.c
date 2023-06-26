e0ee21e33d831439dbe0c307a4422ab4685a7807
Taavetti Yilnan <dyildum@gmail.com>
2023-06-26 14:56:17 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,21 +5,19 @@
  * @str: The string to be treated
  * Return: void
  */
-
 void puts2(char *str)
-
 {
-	int i;
-	int j = 0;
+    int i;
+    int j = 0;
 
-	while (str[j] != '0\')
-	{
-	j++;
-	}
+    while (str[j] != '\0')
+    {
+        j++;
+    }
 
-	for (i = 0; i < j; i += 2)
-	{
-	_putchar(str[i]);
-	}
-	_putchar('\n');
+    for (i = 0; i < j; i += 2)
+    {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
 }
