3b8c4e63e863dcf98b6d9179ba75b39d6bf3cf91
Taavetti Yilnan <dyildum@gmail.com>
2023-07-07 13:32:06 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,14 +4,14 @@
 /**
  * main - Prints its name
  * @argc: argument count
- * @argc: argument vector
+ * @argv: argument vector
  *
  * Return: Always zero
  */
 int main(int argc, char *argv[])
 {
 	(void) argc;
-	printf ("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
