e9283a165f90eb52445e8bbd91aaf10a918cc49f
yildumd <dyildum@gmail.com>
2023-08-03 13:39:24 -0700

    0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int v;
+	unsigned int dec_val = 0;
+
+	if (!b)
+		return (0);
+
+	for (v = 0; b[v]; v++)
+	{
+		if (b[v] < '0' || b[v] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[v] - '0');
+	}
+
+	return (dec_val);
+}
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int v, count = 0;
+	unsigned long int current;
+
+	for (v = 63; v >= 0; v--)
+	{
+		current = n >> v;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
